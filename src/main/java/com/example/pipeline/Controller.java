0f22cb4127cd3a3f21b9e3bd0f7c91a910f9fe97
danielszakacs <daniel.sokach@gmail.com>
2019-01-19 16:04:30 +0100

    new files added

---
+++ src/main/java/com/example/pipeline/Controller.java
@@ -0,0 +1,15 @@
+package com.example.pipeline;
+
+import org.springframework.web.bind.annotation.CrossOrigin;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@CrossOrigin
+@RestController
+public class Controller {
+
+    @GetMapping("/")
+    public String fistPage() {
+        return "Hello Pipeline";
+    }
+}
